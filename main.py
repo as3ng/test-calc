cacaeac78a54e532f9caff79301048610c7331c8
IniPeik <felix.deegawijaya1@gmail.com>
2021-12-22 14:19:43 +0700

    perkalian ditambahkan

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 def tambah(a,b):
 	return a+b
 
+def kurang(a,b):
+	return a-b
+
+def kali(a,b):
+	return a*b
+
 def main():
 	print(tambah(10,5))
-
+	print(kurang(10,5))
+	print(kali(10,5))
 main()
